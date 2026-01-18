55a31bc90343e02ebfe9f6f9497f81557334dd7c
pilosoLaptop <luismoranluiss@gmail.com>
2026-01-17 19:01:03 -0600

    header disseapering

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,20 @@ if (contactForm) {
         }, 1500);
     });
 }
+
+// --- Scroll-to-Hide Logic ---
+let lastScrollY = window.scrollY;
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    // If the current scroll is greater than the last, the user is scrolling down
+    if (lastScrollY < window.scrollY) {
+        nav.classList.add('nav-hidden');
+    } else {
+        // If the current scroll is less, the user is scrolling up
+        nav.classList.remove('nav-hidden');
+    }
+    
+    // Update the last scroll position
+    lastScrollY = window.scrollY;
+});
